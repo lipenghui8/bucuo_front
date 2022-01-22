33b3f7591d488e1689e1638a81995bf298217e7e
lipenghuihenu <493284461@qq.com>
2022-01-22 20:57:55 +0800

    会员中心

--- components/mylist/course.vue
+++ components/mylist/course.vue
@@ -108,16 +108,7 @@
 				<swiper-item class="swiper-item">
 					<scroll-view style="height: 100%;width: 100%;">
 						<view class="page-box">
-							<view>
-								<view class="centre">
-									<image src="http://cdn.zhoukaiwen.com/noData1.png" mode="widthFix"></image>
-									<view class="explain">
-										暂无班级信息
-										<view class="tips">可以去看看有其他课程</view>
-									</view>
-									<view class="btn">随便逛逛</view>
-								</view>
-							</view>
+							<vip/>
 						</view>
 					</scroll-view>
 				</swiper-item>
@@ -127,180 +118,185 @@
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			percent: 50,
-			activeColor: '#0081ff',
-			striped: false,
-			stripedActive: false,
-			
-			avatar: [
-				'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg',
-				'https://ossweb-img.qq.com/images/lol/web201310/skin/big81005.jpg',
-				'https://ossweb-img.qq.com/images/lol/web201310/skin/big25002.jpg',
-				'https://ossweb-img.qq.com/images/lol/web201310/skin/big91012.jpg'
-			],
-			
-			orderList: [[], [], [], []],
-			dataList: [
-				{
-					id: 1,
-					store: '爱情与现代公司理论',
-					progre: 60,
-					goodsList: [
-						{
-							goodsUrl: 'http://cdn.zhoukaiwen.com/web2.jpg',
-							title: '第一阶段·5课时',
-							type: '课程名称：《vue-router全局导航守卫》，简介：导航首位就是变化的路由钩子...',
-							teacher: '王老师',
-							number: 2
-						}
-					]
-				},
-				{
-					id: 2,
-					store: '西南财经大学保研经验贴',
-					progre: 39,
-					goodsList: [
-						{
-							goodsUrl: 'http://cdn.zhoukaiwen.com/web1.jpg',
-							title: 'Promise的链式调用',
-							type: 'Promise 必须为以下三种状态之一：等待态（Pending）、执行态（Fulfilled）和拒绝态（Rejected）。一旦Promise 被 resolve 或 reject，不能再迁移至其他任何状态（即状态 immutable）。',
-							teacher: 'Lee老师',
-							number: 1
-						}
-					]
-				},
-				{
-					id: 3,
-					store: '爱情与现代公司理论',
-					progre: 55,
-					goodsList: [
-						{
-							goodsUrl: 'http://cdn.zhoukaiwen.com/web2.jpg',
-							title: '基于vue应用中实用的性能技巧',
-							type: '1.封装项目的基础库；2.层级管理；3.缓存；4.延迟加载；5.工程化-webpack的改进；6.控制代码质量；7.自动化部署等优化方案',
-							teacher: '王老师',
-							number: 3
-						}
-					]
-				},
-				{
-					id: 4,
-					store: '爱情与现代公司理论',
-					progre: 80,
-					goodsList: [
-						{
-							goodsUrl: 'http://cdn.zhoukaiwen.com/web1.jpg',
-							title: '变量提升及函数提升',
-							type: '引擎在读取js代码的过程中,分为两步。第一个步骤是整个js代码的解析读取,第二个步骤是执行',
-							teacher: '王老师',
-							number: 9
-						}
-					]
-				}
-			],
-			list: [
-				{
-					name: '我的创作'
-				},
-				{
-					name: '我的收藏'
-				},
-				{
-					name: '我的技能'
-				},
-				{
-					name: '会员中心'
-				}
-			],
-			current: 0,
-			swiperCurrent: 0,
-			tabsHeight: 0,
-			dx: 0,
-			loadStatus: ['loadmore','loadmore','loadmore','loadmore'],
-		};
-	},
-	mounted() {
-		this.getOrderList(0);
-		this.getOrderList(1);
-		this.getOrderList(3);
-	},
-	computed: {
-		// 价格小数
-		priceDecimal() {
-			return val => {
-				if (val !== parseInt(val)) return val.slice(-2);
-				else return '00';
+	import vip from "../vip/vip.vue"
+
+	export default {
+		components:{
+			vip
+		},
+		data() {
+			return {
+				percent: 50,
+				activeColor: '#0081ff',
+				striped: false,
+				stripedActive: false,
+				
+				avatar: [
+					'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg',
+					'https://ossweb-img.qq.com/images/lol/web201310/skin/big81005.jpg',
+					'https://ossweb-img.qq.com/images/lol/web201310/skin/big25002.jpg',
+					'https://ossweb-img.qq.com/images/lol/web201310/skin/big91012.jpg'
+				],
+				
+				orderList: [[], [], [], []],
+				dataList: [
+					{
+						id: 1,
+						store: '爱情与现代公司理论',
+						progre: 60,
+						goodsList: [
+							{
+								goodsUrl: 'http://cdn.zhoukaiwen.com/web2.jpg',
+								title: '第一阶段·5课时',
+								type: '课程名称：《vue-router全局导航守卫》，简介：导航首位就是变化的路由钩子...',
+								teacher: '王老师',
+								number: 2
+							}
+						]
+					},
+					{
+						id: 2,
+						store: '西南财经大学保研经验贴',
+						progre: 39,
+						goodsList: [
+							{
+								goodsUrl: 'http://cdn.zhoukaiwen.com/web1.jpg',
+								title: 'Promise的链式调用',
+								type: 'Promise 必须为以下三种状态之一：等待态（Pending）、执行态（Fulfilled）和拒绝态（Rejected）。一旦Promise 被 resolve 或 reject，不能再迁移至其他任何状态（即状态 immutable）。',
+								teacher: 'Lee老师',
+								number: 1
+							}
+						]
+					},
+					{
+						id: 3,
+						store: '爱情与现代公司理论',
+						progre: 55,
+						goodsList: [
+							{
+								goodsUrl: 'http://cdn.zhoukaiwen.com/web2.jpg',
+								title: '基于vue应用中实用的性能技巧',
+								type: '1.封装项目的基础库；2.层级管理；3.缓存；4.延迟加载；5.工程化-webpack的改进；6.控制代码质量；7.自动化部署等优化方案',
+								teacher: '王老师',
+								number: 3
+							}
+						]
+					},
+					{
+						id: 4,
+						store: '爱情与现代公司理论',
+						progre: 80,
+						goodsList: [
+							{
+								goodsUrl: 'http://cdn.zhoukaiwen.com/web1.jpg',
+								title: '变量提升及函数提升',
+								type: '引擎在读取js代码的过程中,分为两步。第一个步骤是整个js代码的解析读取,第二个步骤是执行',
+								teacher: '王老师',
+								number: 9
+							}
+						]
+					}
+				],
+				list: [
+					{
+						name: '我的创作'
+					},
+					{
+						name: '我的收藏'
+					},
+					{
+						name: '我的技能'
+					},
+					{
+						name: '会员中心'
+					}
+				],
+				current: 0,
+				swiperCurrent: 0,
+				tabsHeight: 0,
+				dx: 0,
+				loadStatus: ['loadmore','loadmore','loadmore','loadmore'],
 			};
 		},
-		// 价格整数
-		priceInt() {
-			return val => {
-				if (val !== parseInt(val)) return val.split('.')[0];
-				else return val;
-			};
-		}
-	},
-	methods: {
-		goClass(){
-			uni.showToast({
-			    title: '没有权限～',
-			    duration: 2000,
-				icon: 'none'
-			});
+		mounted() {
+			this.getOrderList(0);
+			this.getOrderList(1);
+			this.getOrderList(3);
 		},
-		reachBottom() {
-			// 此tab为空数据
-			if(this.current != 2) {
-				this.loadStatus.splice(this.current,1,"loading")
-				setTimeout(() => {
-					this.getOrderList(this.current);
-				}, 1200);
+		computed: {
+			// 价格小数
+			priceDecimal() {
+				return val => {
+					if (val !== parseInt(val)) return val.slice(-2);
+					else return '00';
+				};
+			},
+			// 价格整数
+			priceInt() {
+				return val => {
+					if (val !== parseInt(val)) return val.split('.')[0];
+					else return val;
+				};
 			}
 		},
-		// 页面数据
-		getOrderList(idx) {
-			for(let i = 0; i < 5; i++) {
-				let index = this.$u.random(0, this.dataList.length - 1);
-				let data = JSON.parse(JSON.stringify(this.dataList[index]));
-				data.id = this.$u.guid();
-				this.orderList[idx].push(data);
+		methods: {
+			goClass(){
+				uni.showToast({
+					title: '没有权限～',
+					duration: 2000,
+					icon: 'none'
+				});
+			},
+			reachBottom() {
+				// 此tab为空数据
+				if(this.current != 2) {
+					this.loadStatus.splice(this.current,1,"loading")
+					setTimeout(() => {
+						this.getOrderList(this.current);
+					}, 1200);
+				}
+			},
+			// 页面数据
+			getOrderList(idx) {
+				for(let i = 0; i < 5; i++) {
+					let index = this.$u.random(0, this.dataList.length - 1);
+					let data = JSON.parse(JSON.stringify(this.dataList[index]));
+					data.id = this.$u.guid();
+					this.orderList[idx].push(data);
+				}
+				this.loadStatus.splice(this.current,1,"loadmore")
+			},
+			// 总价
+			totalPrice(item) {
+				let price = 0;
+				item.map(val => {
+					price += parseFloat(val.price);
+				});
+				return price.toFixed(2);
+			},
+			// 总件数
+			totalNum(item) {
+				let num = 0;
+				item.map(val => {
+					num += val.number;
+				});
+				return num;
+			},
+			// tab栏切换
+			change(index) {
+				this.swiperCurrent = index;
+				this.getOrderList(index);
+			},
+			transition({ detail: { dx } }) {
+				this.$refs.tabs.setDx(dx);
+			},
+			animationfinish({ detail: { current } }) {
+				this.$refs.tabs.setFinishCurrent(current);
+				this.swiperCurrent = current;
+				this.current = current;
 			}
-			this.loadStatus.splice(this.current,1,"loadmore")
-		},
-		// 总价
-		totalPrice(item) {
-			let price = 0;
-			item.map(val => {
-				price += parseFloat(val.price);
-			});
-			return price.toFixed(2);
-		},
-		// 总件数
-		totalNum(item) {
-			let num = 0;
-			item.map(val => {
-				num += val.number;
-			});
-			return num;
-		},
-		// tab栏切换
-		change(index) {
-			this.swiperCurrent = index;
-			this.getOrderList(index);
-		},
-		transition({ detail: { dx } }) {
-			this.$refs.tabs.setDx(dx);
-		},
-		animationfinish({ detail: { current } }) {
-			this.$refs.tabs.setFinishCurrent(current);
-			this.swiperCurrent = current;
-			this.current = current;
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss" scoped>
