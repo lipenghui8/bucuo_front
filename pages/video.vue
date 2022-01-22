61bd7f99965b2b2710c4d34af9ad6cff722d866f
lipenghuihenu <493284461@qq.com>
2022-01-22 17:44:10 +0800

    修改配置

--- pages/video.vue
+++ pages/video.vue
@@ -4,7 +4,7 @@
 		<view class="MainBox">
 			<!-- 此处为“腾讯视频插件” -->
 			<!-- 小程序没有影视音证书就无法上线视频播放相关功能，必须要使用腾讯视频插件，您如果是做demo不考虑上线，可以使用<video>标签 -->
-			<txv-video :vid="videoUrl" playerid="txv1"></txv-video>
+			<!-- <txv-video :vid="videoUrl" playerid="txv1"></txv-video> -->
 			
 			<view class="cssBox" style="position: relative;">
 				
