5dc17783b8049bd0ca65bcbf3648c01a8cea5d0e
MyPC <2498653944@qq.com>
2022-02-07 15:46:18 +0800

    布局修改

--- pages/function/lost.vue
+++ pages/function/lost.vue
@@ -4,31 +4,27 @@
 			<block slot="backText">返回</block>
 			<block slot="content">失物招领</block>
 		</cu-custom>	
-		<view class="wrap">
+		<view class="wrap" style="margin: 32rpx;">
 			<!-- 注意，如果需要兼容微信小程序，最好通过setRules方法设置rules规则 -->
-			<view class="bg-white solid-bottom text-center padding">
-				<view class="action  ">
-					<text class="cuIcon-titles text-orange">
-					登记须知
-					</text> 
-				</view>
-				</view>
 				<view class="case flex-sub text-left shadow bg-white">
-					<view class="text-black text-bold ">你需要做什么？</view>
-					<view>1.填写学号或ID号</view>
-					<view>2.填写物品名称</view>
-					<view>3.填写放置地点</view>
-					<view class="text-black text-bold">不错校园会做什么？</view>
-					<view>如果失主是星会员,失主将收到手机短信提醒</view>
-					<view>如果失主是其他用户,失主将在小程序内收到提醒</view>
-					<view>当失主确认收到物品后,你将获得1芝士币（上限10）</view>				
-					<view>此外,失主也可选择向你赠送星会员/普通会员</view>				
+					<view class="cuIcon-titles text-orange text-center">
+					登记须知
+					</view> 
+					<view class="text-black text-item text-bold ">你需要做什么？</view>
+					<view class="text text-item" >1.填写学号或ID号</view>
+					<view class="text text-item">2.填写物品名称</view>
+					<view class="text text-item">3.填写放置地点</view>
+					<view class="text-black text-bold text-item">不错校园会做什么？</view>
+					<view class="text text-item">如果失主是星会员,失主将收到手机短信提醒</view>
+					<view class="text text-item">如果失主是其他用户,失主将在小程序内收到提醒</view>
+					<view class="text text-item">当失主确认收到物品后,你将获得1芝士币（上限10）</view>				
+					<view class="text text-item">此外,失主也可选择向你赠送星会员/普通会员</view>				
 				</view>
 				<view 
 				class="case flex-sub text-center shadow bg-white"
-				style="margin-top: 30rpx;"
+				style="margin-top: 30rpx; border-radius: 20rpx;"
 				>
-					<view class="action  ">
+					<view>
 						<text class="cuIcon-titles text-orange">
 						登记单
 						</text> 
@@ -36,26 +32,34 @@
 					<u-form>
 						<u-form-item
 						label="学号  "
+						label-width="80px"
 						>
 							<u-input></u-input>
 						</u-form-item>
 						<u-form-item
-						label="ID   ">
+						label="ID   "
+						label-width="80px"
+						>
 							<u-input></u-input>
 						</u-form-item>
 						<u-form-item
 						label="物品名称"
+						label-width="80px"
 						>
 							<u-input></u-input>
 						</u-form-item>
 						<u-form-item
 						label="存放地点"
+						label-width="80px"
+						
 						>
 							<u-input></u-input>
 						</u-form-item>
 					</u-form>
 					<uni-button
 					class="cu-btn round bg-cyan lg"
+					style="width: 100%;"
+					
 					>提交登记单</uni-button>
 					
 				</view>									
@@ -68,7 +72,7 @@
 export default {
 	data() {
 		return {
-			showSex: false
+			showSex: false,
 	}
 	},
 	methods: {
@@ -76,3 +80,24 @@ export default {
 	},
 };
 </script>
+<style lang="scss" scoped>
+/* #ifndef H5 */
+page {
+	height: 100%;
+	background-color: #f2f2f2;
+}
+/* #endif */
+</style>
+
+<style lang="scss">
+	.text-item{
+		margin-left: 20rpx;
+	}
+	.u-form-item--left__content{
+		background-image: linear-gradient(45deg,#0081ff,#1cbbb4);
+		color: #fff;
+		border-radius: 20rpx;
+		
+	}
+</style>
+
